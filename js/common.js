cebc543a33c7ce87fb9514384c250a50b37e348c
akella <akella.a@gmail.com>
2015-10-09 12:32:59 +0300

    all done

--- js/common.js
+++ js/common.js
@@ -1,19 +1,16 @@
+
 head.ready(function() {
-
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
-
-	// function scrollFixedElements() {
-	//     var scroll_left = $(this).scrollLeft();
-	//     $(".fixed-element").css({
-	//         left: -scroll_left
-	//     });
-	// }
-	// scrollFixedElements();
-	// $(window).scroll(function(){
-	//     scrollFixedElements()
-	// });
-
-	console.log($('body').html());
+	w = $('body').width();
+	$('body').addClass('a');
+	sq1 = $('#logo__sq1');
+	sq2 = $('#logo__sq2');
+	sq3 = $('#logo__sq3');
+	sq4 = $('#logo__sq4');
+	$('body').mousemove(function (e) {
+	    var mouseX = e.pageX;
+	    sq1.css('left',10*mouseX/w);
+	    sq2.css('left',20*mouseX/w);
+	    sq3.css('left',25*mouseX/w);
+	    sq4.css('left',30*mouseX/w);
+	});
 });
