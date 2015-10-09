8d098ee5d30cf974fcd34f1592f80b740bd19876
whats0n <mrdvv@mail.ru>
2015-10-09 15:06:29 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -8,7 +8,7 @@ head.ready(function() {
 	sq4 = $('#logo__sq4');
 
 	function culc(K, BLA, BLABLA){
-		return -(BLA*50-BLABLA*K)
+		return -(BLA*65-BLABLA*K)
 	};
 
 	$('body').mousemove(function (e) {
@@ -18,10 +18,10 @@ head.ready(function() {
 		y = Math.abs(x);
 		console.log(x, y);
 
-    	sq1.css('transform', 'translate('+ culc(10, x, y) +'px,-50%)');
-    	sq2.css('transform', 'translate('+ culc(20, x, y) +'px,-50%)');
-    	sq3.css('transform', 'translate('+ culc(30, x, y) +'px,-50%)');
-    	sq4.css('transform', 'translate('+ culc(40, x, y) +'px,-50%)');
+    	sq1.css('transform', 'translate('+ culc(0, x, y) +'px,-50%)');
+    	sq2.css('transform', 'translate('+ culc(10, x, y) +'px,-50%)');
+    	sq3.css('transform', 'translate('+ culc(20, x, y) +'px,-50%)');
+    	sq4.css('transform', 'translate('+ culc(30, x, y) +'px,-50%)');
 
 	});
 });
