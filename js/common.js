4bb45ad3acc33068ec40eed35081df0ccb87ee27
whats0n <mrdvv@mail.ru>
2015-10-09 13:59:38 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -6,11 +6,22 @@ head.ready(function() {
 	sq2 = $('#logo__sq2');
 	sq3 = $('#logo__sq3');
 	sq4 = $('#logo__sq4');
+
+	function culc(K, BLA, BLABLA){
+		return -(BLA*50-BLABLA*K)
+	};
+
 	$('body').mousemove(function (e) {
 	    var mouseX = e.pageX;
-	    sq1.css('transform', 'translate('+ 10*mouseX/w +'px,-50%)');
-	    sq2.css('transform', 'translate('+ 20*mouseX/w +'px,-50%)');
-	    sq3.css('transform', 'translate('+ 30*mouseX/w +'px,-50%)');
-	    sq4.css('transform', 'translate('+ 40*mouseX/w +'px,-50%)');
+
+		x = mouseX/w-0.5;
+		y = Math.abs(x);
+		console.log(x, y);
+
+    	sq1.css('transform', 'translate('+ culc(10, x, y) +'px,-50%)');
+    	sq2.css('transform', 'translate('+ culc(20, x, y) +'px,-50%)');
+    	sq3.css('transform', 'translate('+ culc(30, x, y) +'px,-50%)');
+    	sq4.css('transform', 'translate('+ culc(40, x, y) +'px,-50%)');
+
 	});
 });
